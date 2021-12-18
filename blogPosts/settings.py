9a69b82ce1c0e22b0deb7ddce4a81053610b4688
Anushil011 <upretianushil011@gmail.com>
2021-12-18 15:54:55 -0600

    Image upload fixed

--- blogPosts/settings.py
+++ blogPosts/settings.py
@@ -120,15 +120,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL= '/images/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static')
+]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-MEDIA_URL= '/files/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'uploads')
-]
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads/')
